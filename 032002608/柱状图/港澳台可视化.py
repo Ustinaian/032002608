05f8803121be7b3266092c0ea03d68bf0d240a2c
Ustinaian <38817285@qq.com>
2022-09-20 01:25:18 +0800

    可视化柱状图‘

---
+++ 032002608/柱状图/港澳台可视化.py
@@ -0,0 +1,43 @@
+import os
+import xlrd
+from pyecharts.charts import Bar
+from pyecharts import options as opts
+
+
+def visualize(filename):
+    basename = os.path.splitext(filename)[0]
+    data = xlrd.open_workbook(filename)
+
+    table = data.sheets()[0]
+
+    areas = []
+    datas = []
+
+    for i in range(1, table.nrows):
+        # 把第几行拿出来作为一个列表
+        a = table.row_values(i)
+        area = a[0]
+        areas.append(area)
+        da = a[1]
+        datas.append(da)
+
+    # 创建一个柱状图对象
+    bar = Bar()
+    # 设置x轴
+    bar.add_xaxis(areas)
+    # 设置y轴和图标名
+    y_title = table.row_values(0)
+    bar.add_yaxis(y_title[1], datas)
+    # 添加options
+    bar.set_global_opts(title_opts=opts.TitleOpts(title=basename))
+
+    # 输出html文件来显示柱状图
+    bar.render('D:/郭君濠/软件工程/第一次编程作业/可视化/柱状图/港澳台累计数据柱状图/' + basename + '.html')
+    print(basename)
+
+
+if __name__ == '__main__':
+    spath = 'D:/郭君濠/软件工程/第一次编程作业/提取数据/港澳台累计确诊'
+    files = os.listdir(spath)
+    for file in files:
+        visualize(file)
